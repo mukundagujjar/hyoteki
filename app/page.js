c95a453ba6abcf256d85d0d94e0429378dce644b
mukundagujjar <mukundagujjar@gmail.com>
2025-01-27 22:38:13 +0530

    goodnight

--- app/page.js
+++ app/page.js
@@ -22,12 +22,12 @@ const RootPage = () => {
 
   return (
     // Main Root Page
-    <div className="flex flex-col p-8 items-center h-full w-full overflow-y-auto">
+    <div className="flex flex-col p-8 items-center justify-center h-full w-full overflow-y-auto">
 
       {/* Navigation to practice section */}
       <div className="flex flex-col items-center gap-10 w-full md:w-[80%]">
         <p className=" text-4xl md:text-6xl font-black text-center">What would you like to practice?</p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full mb-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full mb-40">
           {
             PracticeModes.map((mode) => (
               <PracticeCard key={mode.title} title={mode.title} live={mode.live} href={mode.href} />
@@ -37,7 +37,7 @@ const RootPage = () => {
       </div>
 
       {/* Footer */}
-      <div className="flex flex-col items-center justify-center w-full mt-auto text-xl font-extralight">
+      <div className="flex flex-col items-center justify-center w-full text-xl font-extralight">
         <p>Built by ムクンダ</p>
       </div>
 
